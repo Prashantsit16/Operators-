7b0d2345e773f96f6f91ca5dad6d8cc0bd9d2434
Prashantsit16 <prashant.bajpai.btech2023@sitpune.edu.in>
2024-08-10 17:27:31 +0530

    Add files via upload

---
+++ arithmetic_operations.cpp
@@ -0,0 +1,20 @@
+//Prashant
+//23070123102
+//arithmetic operations 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int A,B;
+    cout<<"Enter 1st number: ";
+    cin>>A;
+    cout<<"Enter 2nd number: ";
+    cin>>B;
+    cout<<"Addition of "<<A<<" and "<<B <<" is: "<<(A+B)<<endl;;
+    cout<<"Subtraction of "<<A<<" and "<<B <<" is: "<<(A-B)<<endl;;
+    cout<<"Multiplication of "<<A<<" and "<<B <<" is: "<<(A*B)<<endl;;
+    cout<<"Division of "<<A<<" and "<<B <<" is: "<<(A/B)<<endl;
+    cout<<"Modulus of "<<A<<" and "<<B <<" is: "<<(A%B)<<endl;
+    return 0;
+
+}
---
+++ operators.cpp
@@ -0,0 +1,67 @@
+//Prashant bajpai
+//23070123102
+//comparison operator 
+#include<iostream>
+using namespace std;
+/**int main()
+{
+    int a,b;
+    cout<<"Enter value of a: ";
+    cin>>a;
+    cout<<"Enter value of b: ";
+    cin>>b;
+     cout<<"a==b : "<<(a==b)<<endl;
+    cout<<"a!=b: "<<(a!=b)<<endl;
+    cout<<"a>b: "<<(a>b)<<endl;
+    cout<<"a<b: "<<(a<b)<<endl;
+    cout<<"a>=b: "<<(a>=b)<<endl;
+    cout<<"a<=b: "<<(a<=b)<<endl;
+    cout<<"a!=b: "<<(a!=b)<<endl;
+    return 0;
+    
+}**/
+
+
+//for getting output as true and false
+/**int main()
+{
+    int a,b;
+    cout<<"Enter value of a: ";
+    cin>>a;
+    cout<<"Enter value of b: ";
+    cin>>b;
+    cout<<boolalpha;
+    cout<<"a==b : "<<(a==b)<<endl;
+    cout<<"a!=b: "<<(a!=b)<<endl;
+    cout<<"a>b: "<<(a>b)<<endl;
+    cout<<"a<b: "<<(a<b)<<endl;
+    cout<<"a>=b: "<<(a>=b)<<endl;
+    cout<<"a<=b: "<<(a<=b)<<endl;
+    cout<<"a!=b: "<<(a!=b)<<endl;
+    return 0;
+
+}*/
+
+//logical operators
+/**int main()
+{
+    bool A=true,b=false;
+    cout<<boolalpha;
+    cout<<"a&&b: "<<(A&&b)<<endl;
+    cout<<"a||b: "<<(A||b)<<endl;
+    return 0;
+
+}*/
+
+// logical operators
+int main()
+{
+    int a=2,b=3;
+    cout<<boolalpha;
+    cout<<"a&b: "<<(a&b)<<endl;
+    cout<<"a|b: "<<(a|b)<<endl;
+    cout<<"a^b: "<<(a^b)<<endl;
+    cout<<"a<<b: "<<(a<<b)<<endl;
+    cout<<"a>>b: "<<(a>>b)<<endl;
+    return 0;
+}
